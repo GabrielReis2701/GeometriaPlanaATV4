7876983b0cf87494bfbb82de6a0f700b7d7aacc0
Gabriel Reis <62779861+GabrielReis2701@users.noreply.github.com>
2022-08-06 10:45:59 -0400

    Versão com abas

--- app/src/main/java/com/example/geometriaplanaatv4/fragmentGeo.java
+++ app/src/main/java/com/example/geometriaplanaatv4/fragmentGeo.java
@@ -1,67 +1,47 @@
 package com.example.geometriaplanaatv4;
 
-import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class fragmentGeo extends AppCompatActivity {
 
-    private Button bt_traingulo,bt_trapezio,bt_losangulo,bt_calcular;
-    private EditText et_valorB,et_valorA;
-    private FragmentTriangulo fragmentTriangulo;
-    private FragmentTrapezio fragmentTrapezio;
-    private FragmentLosango fragmentLosango;
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_geo);
 
+        smartTabLayout = findViewById(R.id.viewpagertab);
+        viewPager = findViewById(R.id.viewpager);
+
         getSupportActionBar().setElevation(0);
-        bt_traingulo = findViewById(R.id.bt_triangulo);
-        bt_trapezio = findViewById(R.id.bt_trapezio);
-        bt_losangulo = findViewById(R.id.bt_losango);
-        bt_calcular = findViewById(R.id.bt_calcular);
 
 
-        fragmentTriangulo = new FragmentTriangulo();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.frame_conteudo,fragmentTriangulo);
-        fragmentTransaction.commit();
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Triangulo",FragmentTriangulo.class)
+                        .add("Trapezio", FragmentTrapezio.class)
+                        .add("Losango", FragmentLosango.class)
+                        .create()
 
-        bt_traingulo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentTriangulo = new FragmentTriangulo();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.frame_conteudo,fragmentTriangulo);
-                fragmentTransaction.commit();
-            }
-        });
-        bt_trapezio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentTrapezio = new FragmentTrapezio();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.frame_conteudo,fragmentTrapezio);
-                fragmentTransaction.commit();
-            }
-        });
-        bt_losangulo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentLosango = new FragmentLosango();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.frame_conteudo,fragmentLosango);
-                fragmentTransaction.commit();
-            }
-        });
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
 
 
 
